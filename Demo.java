a0a21ca205778385d3cfb2e2f59430f5ea8eb549
Prabhakar Kumar <42499823+prabhakarkumar17@users.noreply.github.com>
2024-06-24 20:34:21 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,49 @@
+//Write a "hello world" java program.
+//Whenever we have to write any java code then we have to write it under "class" 
+//Java is a Object Oriented Programming Lang.
+
+// { } - curly braces    ( ) - parenthesis
+
+//javac Demo.java --- Command for compilation
+//java Demo --- Command to run our java file
+
+//We will write our java prog inside class
+//First letter of any class name should be in Uppercase(capital)
+
+//main() - Entry point for compiler from where it will start the execution of program
+//void - Return type
+//static - We dont have to create object of a static class. We can call class's func and var with the help of class name.
+//public - Anyone can call main() 
+//String args[] - command line argument
+
+//System - class
+//out - output stream
+//print - To print any text in the screen
+class Demo{
+	public static void main(String args[ ]){
+		System.out.print("Hello World...this is our first java program");
+	  //class.object.method(argument)
+	}
+}
+//JAVA is a Object oriented Prog lang.
+
+//Fruits - Colour,Taste,Season,Name ----- Class (In general we are alking about fruit)
+//Mango - Green,Sweet,Summer,Mango ----- Object (Specific name of any fruit)
+
+//Car - Colour, Company, Model ---- Class
+//XUV700 - Black,Mahindra,XUV ---- Object
+
+
+
+/*
+class {
+ 	variable;
+ 	method/function;
+}
+
+To access non-static class's var and func we have to make object of that 
+particular non-static class
+
+*/ 
+
+//TO access
